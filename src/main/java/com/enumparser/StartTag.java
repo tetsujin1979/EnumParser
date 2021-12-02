781f9d7cf18289ecc910ecf5ad3327e8ead07ea6
joe <joe@home.com>
2021-12-02 17:02:33 +0000

    interface for method to handle opening tags

---
+++ src/main/java/com/enumparser/StartTag.java
@@ -0,0 +1,20 @@
+package com.enumparser;
+
+import java.util.Deque;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+
+/**
+ * Interface to process opening tags in an XML file
+ * @param <T> The type of class to process
+ */
+public interface StartTag<T> {
+
+	public abstract void processElement(String namespaceURI, 
+										String lName, 
+										String qName,
+										Attributes attributes,
+										Deque<T> deque) throws SAXException;
+
+}
