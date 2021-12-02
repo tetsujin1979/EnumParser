d9a99a04ab0e50230b4d6ce3acb3eec8df4c9b9e
joe <joe@home.com>
2021-12-02 17:00:13 +0000

    handle the closing tags in books.xml

---
+++ src/main/java/com/enumparser/book/BookEndTag.java
@@ -0,0 +1,102 @@
+package com.enumparser.book;
+
+import java.time.LocalDate;
+import java.util.Deque;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.xml.sax.SAXException;
+
+import com.enumparser.EndTag;
+import com.enumparser.book.model.Book;
+
+/**
+ * Enumeration to handle closing tags in books.xml
+ */
+public enum BookEndTag implements EndTag<Book> {
+
+	AUTHOR("author"){
+		
+		public void processElement(String namespaceURI, 
+								   String sName, 
+								   String qName, 
+								   String characters,
+								   Deque<Book> deque) throws SAXException {
+			
+			Book book = deque.peek();
+			book.setAuthor(characters);
+			
+		}
+
+	},
+	ISBN("isbn"){
+		
+		public void processElement(String namespaceURI, 
+								   String sName, 
+								   String qName, 
+								   String characters,
+								   Deque<Book> deque) throws SAXException {
+			
+			Book book = deque.peek();
+			book.setIsbn(characters);
+			
+		}
+
+	},
+	PUBLISH_DATE("publishDate"){
+		
+		public void processElement(String namespaceURI, 
+								   String sName, 
+								   String qName, 
+								   String characters,
+								   Deque<Book> deque) throws SAXException {
+			
+			Book book = deque.peek();
+			LocalDate publishDate = LocalDate.parse(characters);
+			book.setPublishDate(publishDate);
+			
+		}
+
+	},
+	TITLE("title") {
+		
+		public void processElement(String namespaceURI, 
+								   String sName, 
+								   String qName, 
+								   String characters,
+								   Deque<Book> deque) throws SAXException {
+			
+			Book book = deque.peek();
+			book.setTitle(characters);
+			
+		}
+
+	};
+	
+	private String tagName;
+
+	// Map of tag names to an enumeration value to process that tag
+	private static final Map<String, BookEndTag> ENUM_MAP =
+            Stream.of(values())
+                  .collect(Collectors.toMap(BookEndTag::getTagName, e -> e));
+
+	BookEndTag(String tagName) {
+		
+		this.tagName = tagName;
+				
+	}
+	
+	public String getTagName() {
+		
+		return tagName;
+		
+	}
+	
+	public static BookEndTag fromTagName(String tagName) {
+		
+		return ENUM_MAP.get(tagName);
+		
+	}
+ 
+}
