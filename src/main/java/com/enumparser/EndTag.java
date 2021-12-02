a71a3f5df9a7d0871cd6ce28d095ec3449c1b714
joe <joe@home.com>
2021-12-02 17:02:08 +0000

    interface for closing tags

---
+++ src/main/java/com/enumparser/EndTag.java
@@ -0,0 +1,19 @@
+package com.enumparser;
+
+import java.util.Deque;
+
+import org.xml.sax.SAXException;
+
+/**
+ * Interface to process end tags in an xml file
+ * @param <T> The class to process
+ */
+public interface EndTag<T> {
+
+	public abstract void processElement(String namespaceURI, 
+										String sName, 
+										String qName, 
+										String characters,
+										Deque<T> deque) throws SAXException;
+
+}
